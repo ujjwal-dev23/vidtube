267961e98638d70c428971dd1680b078339959fd
Ujjwal <ujjwalkandera23@tutamail.com>
2025-06-08 10:24:14 +0530

    create comment controller and router files

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,29 @@
+import { Comment } from "../models/comment.models.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  const { videoId } = req.params
+  const { page = 1, limit = 10 } = req.query
+
+})
+
+const addComment = asyncHandler(async (req, res) => {
+  // TODO: add a comment to a video
+})
+
+const updateComment = asyncHandler(async (req, res) => {
+  // TODO: update a comment
+})
+
+const deleteComment = asyncHandler(async (req, res) => {
+  // TODO: delete a comment
+})
+
+export {
+  getVideoComments,
+  addComment,
+  updateComment,
+  deleteComment
+}
